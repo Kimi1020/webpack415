c03c441411ef00fa00d12ee5098886e16bf0b049
Wei Li <lwsh@cn.ibm.com>
2018-07-06 11:36:45 +0800

    Project Initiation

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var _ = require('lodash');
+function component() {
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+}
+document.body.appendChild(component());
