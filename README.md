5f872409cfc570d8017186e1d77365b2c0cd574d
Wei Li <lwsh@cn.ibm.com>
2018-07-06 16:21:34 +0800

    Asset Management

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ To-do list
 =======================
 1. Add Bable via webpack's loader system
 2. Add minimizing css for production  (minimize css for better load times in production)
+3. Add minifying and optimizing the images. image-webpack-loder url-loader
 
 
 Learning List
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import mapVol from './mapVol.png';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    //Add the image to our existing div.
+    var myIcon = new Image();
+    myIcon.src = mapVol;
+    element.appendChild(myIcon);
+    console.log(Data);
     return element;
 }
 document.body.appendChild(component());
