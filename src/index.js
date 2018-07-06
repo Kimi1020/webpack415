b8d2c8c4f7837105fa2dce5e846184a933e973a6
Wei Li <lwsh@cn.ibm.com>
2018-07-06 14:55:10 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-var _ = require('lodash');
+import _ from 'lodash';
+
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
