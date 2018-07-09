4365e9088c57311600c0459025d073e368af68e9
Wei Li <lwsh@cn.ibm.com>
2018-07-09 10:04:02 +0800

    Before Developement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import './style.css';
-import mapVol from './mapVol.png';
-import Data from './data.xml';
+import printMe from './print.js';
+// import mapVol from './mapVol.png';
+// import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
@@ -9,10 +10,15 @@ function component() {
     element.classList.add('hello');
 
     //Add the image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = mapVol;
-    element.appendChild(myIcon);
-    console.log(Data);
+    // var myIcon = new Image();
+    // myIcon.src = mapVol;
+    // element.appendChild(myIcon);
+    // console.log(Data);
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'click me and check the console';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 document.body.appendChild(component());
